23597f7b68073b77ba293b25feb0d781ee5dc1d9
Patrik Jira <patrikjira@gmail.com>
2012-03-16 20:51:53 +0100

    Nicer attachable migration

--- db/migrate/20120316190255_add_attachable_to_galleries.etabliocms_galleries_engine.rb
+++ db/migrate/20120316190255_add_attachable_to_galleries.etabliocms_galleries_engine.rb
@@ -1,13 +1,8 @@
 class AddAttachableToGalleries < ActiveRecord::Migration
 
-  def up
+  def change
     add_column :galleries, :attachable_type, :string
     add_column :galleries, :attachable_id, :integer
-  end
-
-  def down
-    remove_column :galleries, :attachable_type
-    remove_column :galleries, :attachable_id
   end
 
 end
--- spec/dummy/db/migrate/20120316183706_add_attachable_to_galleries.etabliocms_galleries_engine.rb
+++ spec/dummy/db/migrate/20120316183706_add_attachable_to_galleries.etabliocms_galleries_engine.rb
@@ -1,14 +1,9 @@
 # This migration comes from etabliocms_galleries_engine (originally 20120316190255)
 class AddAttachableToGalleries < ActiveRecord::Migration
 
-  def up
+  def change
     add_column :galleries, :attachable_type, :string
     add_column :galleries, :attachable_id, :integer
-  end
-
-  def down
-    remove_column :galleries, :attachable_type
-    remove_column :galleries, :attachable_id
   end
 
 end
